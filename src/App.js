04b58c8157c28e9272e51268c4081c5c6a34477a
Hieu Truong <hieu.tnxd.bk@gmail.com>
2021-06-19 21:10:01 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Welcome to Hiếu Website</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
